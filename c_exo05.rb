bd91c9b09200bf11cea9ed03a845062f7b5f0e94
corentinpierru <corentin.pierru@laposte.net>
2020-04-07 11:33:44 +0200

    ajout exo05

---
+++ c_exo05.rb
@@ -0,0 +1,31 @@
+puts "On va compter le nombre d'heures de travail à THP"
+#renvoie "on va compter le nombre d'heures de travail à THP"
+puts "Travail : #{10 * 5 * 11}"
+#renvoie le résultat travail avec le résultat grâce au dièse
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
+#renvoie le résultat minutes avec le résultat grâce au dièse
+puts "Et en secondes ?"
+#renvoie et en secondes
+puts 10 * 5 * 11 * 60 * 60
+#pas de guillemet donc pas besoin de mettre en accolade, résultat direct
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+#affiche la question
+puts 3 + 2 < 5 - 7
+#exécution d'un test 
+puts "Ça fait combien 3 + 2 ? #{3 + 2}"
+#renvoie "ca fait combien 3+2 et le résultat grâce aux accolades"
+puts "Ça fait combien 5 - 7 ? #{5 - 7}"
+#renvoie "ca fait combien 5-7 et le résultat grâce aux accolades"
+puts "Ok, c'est faux alors !"
+#renvoie ok c'est faux alors
+puts "C'est drôle ça, faisons-en plus :"
+#renvoie nien nien
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
+#renvoie est-ce que 5 est plus que -2 résultat du test
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
+#pareil
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
+#pareil
+
+
+#les guillemets servent à faire un hash et donc un tableau de donnée
