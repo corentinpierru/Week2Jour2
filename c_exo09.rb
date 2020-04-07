5cab4c2d9b3e878851019bf78455dad16964a688
corentinpierru <corentin.pierru@laposte.net>
2020-04-07 11:51:13 +0200

    ajout exo09

---
+++ c_exo09.rb
@@ -0,0 +1,7 @@
+print "Hey, c'est quoi ton blaz ?"
+user_name = gets.chomp
+puts "Bonjour, #{user_name}"
+print "et ton nom?"
+user_familyname = gets.chomp
+puts "Bonjour, #{user_name} #{user_familyname}"
+
