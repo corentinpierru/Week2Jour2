c90544c842a4b0b6d533416c4751850c013d3f34
corentinpierru <corentin.pierru@laposte.net>
2020-04-07 14:03:53 +0200

    ajout exo15

---
+++ c_exo15.rb
@@ -0,0 +1,9 @@
+puts "Quel est ton année de naissance ?"
+naissance = gets.to_i 
+nombreannee = 2020 - naissance
+age = 0
+(nombreannee).times do 
+  puts "#{naissance = naissance +1} tu as 
+  #{age = age +1}"
+end 
+
