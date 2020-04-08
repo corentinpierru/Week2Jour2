231dc0afb70f1f7c9f27246fa99f7d73597f8604
corentinpierru <corentin.pierru@laposte.net>
2020-04-08 06:43:54 +0200

    leg modif

--- c_exo16.rb
+++ c_exo16.rb
@@ -3,12 +3,15 @@ age = gets.to_i
 nombreannee = age
 date = 0
 (nombreannee).times do 
+  
   if age == date
     puts "il y a #{age} tu avais la moitié de ton âge"
-  end
-else
-  puts "Il y a #{age = age -1} tu avais 
-  #{date = date +1}"
   
-
+else
+  puts "Il y a #{age} tu avais 
+  #{date}"
+  
+end
+date+=1
+age -=1
 end
