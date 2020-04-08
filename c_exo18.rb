345bf47214b62246f393faea8e0ccd379e990ed5
corentinpierru <corentin.pierru@laposte.net>
2020-04-08 06:58:50 +0200

    leg modif exo 18

--- c_exo18.rb
+++ c_exo18.rb
@@ -1,13 +1,19 @@
 mes_emails = []
-50.times do |i = 1|
+50.times do |i|
   if i <10
     i += 1
   mes_emails << "jean.dupont.0#{i}@email.fr"
   
   else
-    i += 1
+    i+=1
   mes_emails << "jean.dupont.#{i}@email.fr"
-  
+
   end
 end
-puts mes_emails
+
+50.times do |i|
+  if i.odd?
+    puts mes_emails[i]
+end
+end
+
